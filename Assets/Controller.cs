6c7c916ca0107aab1d3e086d7eb3dc483494259f
Mark Youssef <mark@Marks-MacBook-Pro.local>
2020-10-17 18:29:11 -0700

    Initial Commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject deathZone;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+    private float moveInput;
+    private float speed = 10f;
+
+    public Text scoreText;
+    public Text startText;
+    public Text deadText;
+    private float topScore = 0.0f;
+    private bool isStarted = false;
+    public bool isDead = false;
+    private float angle = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.gravityScale = 0;
+        rb2d.velocity = Vector3.zero;
+
+        spriteRenderer = player.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isStarted == false)
+        {
+            isStarted = true;
+            startText.gameObject.SetActive(false);
+            rb2d.gravityScale = 5f;
+        }
+
+        if (isStarted)
+        {
+            if (rb2d.velocity.y > 0 && player.transform.position.y > topScore)
+            {
+                topScore = player.transform.position.y;
+            }
+
+            scoreText.text = "Score: " + Mathf.Round(topScore).ToString();
+
+            if (rb2d.velocity.y > 0)
+            {
+                deathZone.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + -25f, player.transform.position.z);
+            }
+        }
+
+        if (isDead)
+        {
+            deadText.gameObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isStarted)
+        {
+            moveInput = Input.GetAxis("Horizontal");
+            rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
+
+            if (moveInput < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            if (moveInput > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+
+            Vector2 v = rb2d.velocity;
+            angle = Mathf.Atan2(v.x, v.y) * Mathf.Rad2Deg;
+            player.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}
---
+++ Assets/Destroy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject platformPrefab;
+    public GameObject springPrefab;
+    private GameObject myPlat;
+    public Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x + offset.x, player.transform.position.y + offset.y, player.transform.position.z);
+        // box.offset = new Vector2(player.transform.position.x + offset.x, player.transform.position.y + offset.y);
+    }
+
+    private void move(Collider2D collision)
+    {
+        collision.gameObject.transform.position = new Vector2(Random.Range(-6f, 6f), player.transform.position.y + (14 + Random.Range(0.5f, 1f)));
+    }
+
+    private void replace(Collider2D collision, GameObject prefab)
+    {
+        Destroy(collision.gameObject);
+        Instantiate(prefab, new Vector2(Random.Range(-6f, 6f), player.transform.position.y + (14 + Random.Range(0.5f, 1f))), Quaternion.identity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name.StartsWith("Platform"))
+        {
+            if (Random.Range(1, 7) == 1)
+            {
+                replace(collision, springPrefab);
+            }
+            else
+            {
+                move(collision);
+            }
+        }
+        else if (collision.gameObject.name.StartsWith("Spring"))
+        {
+            if (Random.Range(1, 7) == 1)
+            {
+                move(collision);
+            }
+            else
+            {
+                replace(collision, platformPrefab);
+            }
+        }
+    }
+}
